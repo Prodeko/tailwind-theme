83ca9d269a22a67fc224722c74aa24b53f1216a5
Risto Virtaharju <rvirtaha@hotmail.com>
2025-05-09 21:33:22 +0300

    feat: add Raleway font

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const plugin = require('tailwindcss/plugin')
+const defaultTheme = require('tailwindcss/defaultTheme')
+const faces = require('./font-faces.js')
 
-module.exports = plugin()
+module.exports = plugin.withOptions(
+    () =>
+        function ({ addBase, theme }) {
+            addBase({
+                '@font-face': faces
+            })
+
+            addBase({
+                html: { fontFamily: theme('fontFamily.raleway') },
+            })
+        },
+    () => ({
+        theme: {
+            extend: {
+                fontFamily: {
+                    raleway: ["Raleway", ...defaultTheme.fontFamily.sans],
+                }
+            }
+        }
+    })
+)
